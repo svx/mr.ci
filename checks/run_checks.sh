245a0aae9b4436a25f478e1543b16acfb88066f5
sven <sven@so36.net>
2016-06-30 13:43:16 +0200

    Wip: travis test2

--- checks/run_checks.sh
+++ checks/run_checks.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-./tests.sh | while read -r line ; do docker run -i --rm -v ${PWD}/docs:/build/docs:rw -u $(id -u):$(id -g) --name docs-test-foo quay.io/tiramisu/mr.docs linkcheck $line; done
+bash checks/test.sh | while read -r line ; do docker run -i --rm -v ${PWD}/docs:/build/docs:rw -u $(id -u):$(id -g) --name docs-test-foo quay.io/tiramisu/mr.docs linkcheck $line; done
